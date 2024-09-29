82f01281abd96645e1c28eb68e65a6a68feb2ba2
Robert Koch <robert@kochie.io>
2024-09-30 01:13:13 +1000

    Adds privacy policy

---
+++ src/Privacy.tsx
@@ -0,0 +1,46 @@
+function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy for MetaFixer</h1>
+      <span>Effective Date: 30 September 2024</span>
+      <p>
+        At MetaFixer, we are committed to protecting your privacy. This privacy
+        policy outlines the types of information we do and do not collect when
+        you use the MetaFixer app.
+      </p>
+      <h2>Data Collection</h2>
+      <p>
+        MetaFixer does not collect, store, or share any personal information.
+      </p>
+      <p>
+        The app operates locally on your device and does not transmit any data
+        to external servers. All data related to your photos and metadata
+        remains on your device and is not accessible to us or any third parties.
+      </p>
+      <h2>Third-Party Services</h2>
+      <p>
+        MetaFixer does not use any third-party analytics, advertising, or data
+        collection services.
+      </p>
+      <h2>Your Consent</h2>
+      <p>
+        By using MetaFixer, you consent to this privacy policy. As we do not
+        collect any data, there is no need for opt-in or opt-out mechanisms.
+      </p>
+      <h2>Changes to This Privacy Policy</h2>
+      <p>
+        Since MetaFixer does not collect any data, it is unlikely that this
+        privacy policy will change. However, we may update this policy to
+        reflect changes in the app’s functionality or legal requirements. Any
+        changes will be posted within the app.
+      </p>
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about this privacy policy, feel free to
+        contact us at robert@kochie.io.
+      </p>
+    </div>
+  );
+}
+
+export default Privacy;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
+import Privacy from './Privacy.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/privacy",
+    element: <Privacy />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

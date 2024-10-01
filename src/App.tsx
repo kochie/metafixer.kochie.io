0b0e33f2eefede53e64e13dc40f1125fe5f2e462
Robert Koch <robert@kochie.io>
2024-10-02 00:50:14 +1000

    updates site

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,135 @@
-import metafixerLogo from './assets/meta_fixer_logo.png'
-import metafixerScreenshot from './assets/meta_fixer_screenshot.png'
-import './App.css'
-import { Button } from '@headlessui/react'
+import metafixerLogo from "./assets/meta_fixer_logo.png";
+import metafixerScreenshot from "./assets/meta_fixer_screenshot.png";
+import Badge from "./assets/download_badge.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpBigSmall,
+  faBoltLightning,
+  faCalendar,
+  faCopyright,
+  faLayerGroup,
+  faTableLayout,
+} from "@fortawesome/pro-duotone-svg-icons";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+
+const features = [
+  { icon: faLayerGroup, text: "Batch processing of Facebook exported photos" },
+  { icon: faCalendar, text: "Adds correct date and location metadata" },
+  {
+    icon: faArrowUpBigSmall,
+    text: "Ensures photos appear in the right order in your photo apps",
+  },
+  { icon: faTableLayout, text: "User-friendly interface for easy navigation" },
+  { icon: faBoltLightning, text: "Fast and efficient processing" },
+  { icon: faApple, text: "Compatible with macOS" },
+];
 
 function App() {
-    return (
-      <div className="min-h-screen bg-gray-100">
-        <header className="bg-white shadow-sm">
-          <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-            <div className="flex items-center">
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+          <div className="flex items-center">
+            <img
+              src={metafixerLogo}
+              alt="MetaFixer Logo"
+              width={50}
+              height={50}
+              className="mr-3"
+            />
+            <h1 className="text-2xl font-bold text-gray-900">MetaFixer</h1>
+          </div>
+          <nav>
+            <Button variant="outline" className="mr-2">
+              <a href="/#key-features">Features</a>
+            </Button>
+            <Button>
+              <a href="/MetaFixer.zip" download="MetaFixer.zip">
+                Download
+              </a>
+            </Button>
+          </nav>
+        </div>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+          <div className="flex flex-col md:flex-row items-center justify-between">
+            <div className="md:w-1/2 mb-6 md:mb-0">
+              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+                Fix your photo metadata with ease
+              </h2>
+              <p className="mt-4 text-lg text-gray-500">
+                MetaFixer is a powerful tool designed to add missing metadata to
+                photos exported from Facebook, ensuring they appear in the
+                correct date and location when imported into photo apps.
+              </p>
+              <div className="mt-6">
+                <a href="" className="hover:">
+                  <img src={Badge} />
+                </a>
+              </div>
+            </div>
+            <div className="md:w-1/2">
               <img
-                src={metafixerLogo}
-                alt="MetaFixer Logo"
-                width={50}
-                height={50}
-                className="mr-3"
+                src={metafixerScreenshot}
+                alt="MetaFixer Screenshot"
+                width={600}
+                height={400}
+                className="rounded-lg shadow-lg"
               />
-              <h1 className="text-2xl font-bold text-gray-900">MetaFixer</h1>
             </div>
-            <nav>
-              <Button className="mr-2">Features</Button>
-              <Button className="mr-2">Pricing</Button>
-              <Button>Download</Button>
-            </nav>
           </div>
-        </header>
-  
-        <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 sm:px-0">
-            <div className="flex flex-col md:flex-row items-center justify-between">
-              <div className="md:w-1/2 mb-6 md:mb-0">
-                <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                  Fix your photo metadata with ease
-                </h2>
-                <p className="mt-4 text-lg text-gray-500">
-                  MetaFixer is a powerful tool designed to add missing metadata to photos exported from Facebook, ensuring they appear in the correct date and location when imported into photo apps.
-                </p>
-                <div className="mt-6">
-                  <Button>Get Started</Button>
-                </div>
-              </div>
-              <div className="md:w-1/2">
-                <img
-                  src={metafixerScreenshot}
-                  alt="MetaFixer Screenshot"
-                  width={600}
-                  height={400}
-                  className="rounded-lg shadow-lg"
-                />
-              </div>
-            </div>
-  
-            <div className="mt-12">
-              <h3 className="text-2xl font-bold text-gray-900 mb-4">Key Features</h3>
-              <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                {[
-                  "Batch processing of Facebook exported photos",
-                  "Adds correct date and location metadata",
-                  "Ensures photos appear in the right order in your photo apps",
-                  "User-friendly interface for easy navigation",
-                  "Fast and efficient processing",
-                  "Compatible with macOS"
-                ].map((feature, index) => (
-                  <li key={index} className="bg-white overflow-hidden shadow rounded-lg">
-                    <div className="px-4 py-5 sm:p-6">
-                      <div className="text-lg leading-6 font-medium text-gray-900">{feature}</div>
+
+          <div className="mt-12">
+            <h3
+              className="text-2xl font-bold text-gray-900 mb-4"
+              id="key-features"
+            >
+              Key Features
+            </h3>
+            <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+              {features.map((feature, index) => (
+                <li
+                  key={index}
+                  className="bg-white overflow-hidden shadow rounded-lg "
+                >
+                  <div className="px-4 py-5 sm:p-6 flex gap-4 items-center h-full">
+                    <FontAwesomeIcon
+                      icon={feature.icon}
+                      fixedWidth
+                      className="text-gray-900"
+                      size="2x"
+                    />
+                    <div className="text-lg leading-6 font-medium text-gray-900">
+                      {feature.text}
                     </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
-        </main>
-  
-        <footer className="bg-white mt-12">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-            <p className="text-gray-500">© 2024 MetaFixer. All rights reserved.</p>
-            <a href="/privacy" className="text-gray-500 hover:text-gray-700 transition-colors">
-              Privacy Policy
-            </a>
-          </div>
-        </footer>
-      </div>
-  )
+        </div>
+      </main>
+
+      <footer className="bg-white mt-12">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+          <p className="text-gray-500">
+            <FontAwesomeIcon icon={faCopyright} /> 2024 MetaFixer. All rights
+            reserved.
+          </p>
+          <a
+            href="/privacy"
+            className="text-gray-500 hover:text-gray-700 transition-colors"
+          >
+            Privacy Policy
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Privacy.tsx
+++ src/Privacy.tsx
@@ -1,44 +1,103 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Button } from "./components/ui/button";
+import metafixerLogo from "@/assets/meta_fixer_logo.png";
+import { faCopyright } from "@fortawesome/pro-duotone-svg-icons";
+
 function Privacy() {
   return (
-    <div>
-      <h1>Privacy Policy for MetaFixer</h1>
-      <span>Effective Date: 30 September 2024</span>
-      <p>
-        At MetaFixer, we are committed to protecting your privacy. This privacy
-        policy outlines the types of information we do and do not collect when
-        you use the MetaFixer app.
-      </p>
-      <h2>Data Collection</h2>
-      <p>
-        MetaFixer does not collect, store, or share any personal information.
-      </p>
-      <p>
-        The app operates locally on your device and does not transmit any data
-        to external servers. All data related to your photos and metadata
-        remains on your device and is not accessible to us or any third parties.
-      </p>
-      <h2>Third-Party Services</h2>
-      <p>
-        MetaFixer does not use any third-party analytics, advertising, or data
-        collection services.
-      </p>
-      <h2>Your Consent</h2>
-      <p>
-        By using MetaFixer, you consent to this privacy policy. As we do not
-        collect any data, there is no need for opt-in or opt-out mechanisms.
-      </p>
-      <h2>Changes to This Privacy Policy</h2>
-      <p>
-        Since MetaFixer does not collect any data, it is unlikely that this
-        privacy policy will change. However, we may update this policy to
-        reflect changes in the app’s functionality or legal requirements. Any
-        changes will be posted within the app.
-      </p>
-      <h2>Contact Us</h2>
-      <p>
-        If you have any questions about this privacy policy, feel free to
-        contact us at robert@kochie.io.
-      </p>
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <header className="bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+          <a href="/">
+            <div className="flex items-center">
+              <img
+                src={metafixerLogo}
+                alt="MetaFixer Logo"
+                width={50}
+                height={50}
+                className="mr-3"
+              />
+              <h1 className="text-2xl font-bold text-gray-900">MetaFixer</h1>
+            </div>
+          </a>
+          <nav>
+            <Button variant="outline" className="mr-2">
+              <a href="/#key-features">Features</a>
+            </Button>
+            <Button>
+              <a href="/MetaFixer.zip" download="MetaFixer.zip">
+                Download
+              </a>
+            </Button>
+          </nav>
+        </div>
+      </header>
+      <main className="max-w-5xl mx-auto py-6 sm:px-6 lg:px-8 flex-1">
+        <div className="">
+          <h1 className="text-3xl font-bold">Privacy Policy for MetaFixer</h1>
+          <span className="text-gray-600 text-sm font-semibold">
+            Effective Date: 30 September 2024
+          </span>
+          <p className="my-2">
+            At MetaFixer, we are committed to protecting your privacy. This
+            privacy policy outlines the types of information we do and do not
+            collect when you use the MetaFixer app.
+          </p>
+          <h2 className="text-2xl font-semibold mb-2">Data Collection</h2>
+          <p className="my-2">
+            MetaFixer does not collect, store, or share any personal
+            information.
+          </p>
+          <p className="my-2">
+            The app operates locally on your device and does not transmit any
+            data to external servers. All data related to your photos and
+            metadata remains on your device and is not accessible to us or any
+            third parties.
+          </p>
+          <h2 className="text-2xl font-semibold mb-2">Third-Party Services</h2>
+          <p className="my-2">
+            MetaFixer does not use any third-party analytics, advertising, or
+            data collection services.
+          </p>
+          <h2 className="text-2xl font-semibold mb-2">Your Consent</h2>
+          <p className="my-2">
+            By using MetaFixer, you consent to this privacy policy. As we do not
+            collect any data, there is no need for opt-in or opt-out mechanisms.
+          </p>
+          <h2 className="text-2xl font-semibold mb-2">
+            Changes to This Privacy Policy
+          </h2>
+          <p className="my-2">
+            Since MetaFixer does not collect any data, it is unlikely that this
+            privacy policy will change. However, we may update this policy to
+            reflect changes in the app’s functionality or legal requirements.
+            Any changes will be posted within the app.
+          </p>
+          <h2 className="text-2xl font-semibold mb-2">Contact Us</h2>
+          <p className="my-2">
+            If you have any questions about this privacy policy, feel free to
+            contact me at{" "}
+            <a href="mailto:robert@kochie.io" className="underline font-medium">
+              robert@kochie.io
+            </a>
+            .
+          </p>
+        </div>
+      </main>
+      <footer className="bg-white mt-12">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+          <p className="text-gray-500">
+            <FontAwesomeIcon icon={faCopyright} /> 2024 MetaFixer. All rights
+            reserved.
+          </p>
+          <a
+            href="/privacy"
+            className="text-gray-500 hover:text-gray-700 transition-colors"
+          >
+            Privacy Policy
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
